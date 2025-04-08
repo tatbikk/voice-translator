e53eafdd1bc1ebcd57ac1a034bcd384dccb2790c
tatbikk <abd199319944@gmail.com>
2025-04-08 16:15:48 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-})
+  root: '.', // جذر المشروع
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: path.resolve(__dirname, 'public/index.html') // هذا هو التعديل المهم
+    }
+  }
+});
